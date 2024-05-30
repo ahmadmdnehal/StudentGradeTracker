167b9849723e3460b47c46c4e0266983187c4f2b
ahmadmdnehal <117920448+ahmadmdnehal@users.noreply.github.com>
2024-05-30 15:51:20 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,37 @@
+package StudentGradeTracker;
+
+public class BankAccount {
+    private String ownerName;
+    private double balance;
+
+    public BankAccount(String ownerName, double initialBalance) {
+        this.ownerName = ownerName;
+        this.balance = initialBalance;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
